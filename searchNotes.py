7efa1ed3bed1bc49faa7c8e050dbbf5a2c8320b2
Rich Vreeland <holler@richvree.land>
2018-07-29 17:10:20 -0500

    Include note contents in match searching, optionally sort results by title.
    Include note contents in match field for better searching, and replace snippets in result subtitles with the beginning of note contents, for more consistent subtitle population. You can optionally sort results by title via an environment variable.

--- searchNotes.py
+++ searchNotes.py
@@ -1,38 +1,59 @@
 #!/usr/bin/python
 import sqlite3
 import json
+import zlib
+import re
+import os
 
+# Sort matches by title or modification date. Edit w/environment variable.
+sortId = 3 if os.getenv('sortBy') != 'Title' else 0
+sortInReverse = sortId is 3
 
 # Open notes database
 home = '/'.join(__file__.split('/')[:3])
-conn = sqlite3.connect(home + '/Library/Group Containers/group.com.apple.notes/NoteStore.sqlite')
+conn = sqlite3.connect(
+    home + '/Library/Group Containers/group.com.apple.notes/NoteStore.sqlite')
 c = conn.cursor()
 
 # Get uuid string required in full id
 c.execute("SELECT z_uuid FROM z_metadata")
 uuid = str(c.fetchone()[0])
 
-# Get tuples of note title, folder code, snippet, modification date, and id number 
-c.execute("""SELECT ztitle1,zfolder,zsnippet,zmodificationdate1,z_pk
-FROM ziccloudsyncingobject 
-WHERE ztitle1 IS NOT NULL AND zmarkedfordeletion IS NOT 1""")
+# Get tuples of note title, folder code, snippet, modification date, & id#
+# 432 is the zfolder id for 'Recently Deleted'
+c.execute("""SELECT t1.ztitle1,t1.zfolder,t1.zsnippet,t1.zmodificationdate1,t1.z_pk,t1.znotedata,t2.zdata,t2.z_pk
+FROM ziccloudsyncingobject AS t1
+INNER JOIN zicnotedata AS t2
+ON t1.znotedata = t2.z_pk
+WHERE t1.ztitle1 IS NOT NULL AND t1.zfolder IS NOT 432 AND t1.zmarkedfordeletion IS NOT 1""")
 matches = c.fetchall()
-# Sort by modification date
-matches = sorted(matches, key=lambda m: m[3], reverse=True)
+matches = sorted(matches, key=lambda m: m[sortId], reverse=sortInReverse)
 
 # Get ordered lists of folder codes and folder names
-c.execute("""SELECT z_pk,ztitle2 
-FROM ziccloudsyncingobject 
+c.execute("""SELECT z_pk,ztitle2
+FROM ziccloudsyncingobject
 WHERE ztitle2 IS NOT NULL AND zmarkedfordeletion IS NOT 1""")
 folderCodes, folderNames = zip(*c.fetchall())
 
 conn.close()
 
-# Alfred results: title=note title, arg=id to pass on, subtitle="folder name | snippet"
-items = [{"title":    m[0], 
-          "arg":      "x-coredata://" + uuid + "/ICNote/p" + str(m[4]), 
-          "subtitle": folderNames[folderCodes.index(m[1])] + " | " + m[2][:80]}
+# Alfred results: title = note title, arg = id to pass on, subtitle = folder name, match = the note contents
+items = [{"title": m[0],
+          "arg": "x-coredata://" + uuid + "/ICNote/p" + str(m[4]),
+          "subtitle": folderNames[folderCodes.index(m[1])],
+          #  + ("  |  " + m[2] if type(m[2]) is unicode and len(m[2]) > 0 else ""),
+          #
+          #  decompress gzipped notes from the sqlite database, strip out gobbledygook footers.
+          "match": zlib.decompress(m[6], 16+zlib.MAX_WBITS).split('\x1a\x10', 1)[0]}
          for m in matches]
+
+# Do further clean up and additions to the match and subtitle fields.
+for i, item in enumerate(items):
+    # strip weird characters, title & weird header artifacts,
+    # replace line breaks with spaces.
+    txt = re.sub('^  ', '', re.sub('\n', ' ', re.sub('^.*\n', ' ', re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]', '', items[i]['match']))))
+    items[i]['match'] = items[i]['title'] + " " + items[i]['subtitle'] + " " + txt
+    items[i]['subtitle'] += "  |  " + txt[:100]
 
 # Custom icons for folder names that start with corresponding emoji
 icons = [u'\ud83d\udcd3', u'\ud83d\udcd5', u'\ud83d\udcd7', u'\ud83d\udcd8', u'\ud83d\udcd9']
@@ -44,4 +65,4 @@ for i in items:
         i['icon'] = {'type': 'image', 'path': 'icons/' + icon.encode('raw_unicode_escape') + '.png'}
 
 output = {"items": items}
-print json.dumps(output)
+print json.dumps(output, sort_keys=True, indent=4, separators=(',', ': '))
