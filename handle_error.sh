0caae33351eeb7e6cb48cffad74b1c4d8d487ce8
Sean Ballinger <sean.ballinger@gmail.com>
2020-05-30 18:01:29 -0400

    Search titles now using Go

--- handle_error.sh
+++ handle_error.sh
@@ -1,8 +1,4 @@
-osascript -e "display dialog \"The Search Notes workflow encountered an error. Press OK to open the error log and the Issues page on Github. Please include the error log output in your bug report.
-
-The workflow requires python 3, which can be installed with this Terminal command:
-
-xcode-select --install\""
+osascript -e "display dialog \"The Search Notes workflow encountered an error. Press OK to open the error log and the issues page on Github. Please include the error log output in your bug report.\""
 if [[ $? == 0 ]]; then
     open "https://github.com/sballin/alfred-search-notes-app/issues"
     open error_log.txt
