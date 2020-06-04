33e7f64b205760439df16e4a9d35bfdef9f34bf6
Sean Ballinger <sean.ballinger@gmail.com>
2020-06-03 20:23:46 -0400

    Better error handling

--- handle_error.sh
+++
@@ -1,6 +0,0 @@
-osascript -e "display dialog \"The Search Notes workflow encountered an error. 
-
-You may need to right click the 'search' binary and click Open for macOS to trust it. Press OK to open the folder containing the binary.\""
-if [[ $? == 0 ]]; then
-    open ./search
-fi
