b144649c4a22f4211228e33d261f4fe10f3d8e6e
Sean Ballinger <sean.ballinger@gmail.com>
2021-03-18 15:30:31 -0400

    Improve update.py

--- update.py
+++ update.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 import os
+import subprocess
 import time
 import json
 import plistlib
@@ -13,7 +14,7 @@ def oneDaySinceLastCheck():
     '''
     lastCheck = os.path.getmtime(__file__)
     if time.time() > 24*60*60 + lastCheck:
-        os.system('touch ' + __file__)
+        subprocess.call(['/usr/bin/touch', __file__])
         return True
     else:
         return False
@@ -23,7 +24,7 @@ def updateAvailable(latestVersion):
     '''
     Check whether the latest version is ahead of the current version.
     '''
-    currentVersion = plistlib.readPlist('info.plist')['version']    
+    currentVersion = plistlib.readPlist(os.path.dirname(__file__) + '/info.plist')['version']    
     if str(currentVersion).lower().strip() == str(latestVersion).lower().strip():
         return False
     else:
@@ -34,11 +35,7 @@ def userWantsUpdate(updateNotes):
     '''
     Show user a confirmation dialog.
     '''
-    retval = os.system("""osascript -e 'display dialog "An update is available for the Alfred Search Notes workflow. You can disable automatic update checks by editing the workflow.
-    
-Information about this release:
-
-%s" with title "Alfred Search Notes Workflow" buttons {"Cancel", "Download"} default button "Download" cancel button "Cancel"' 2>/dev/null""" % updateNotes)
+    retval = subprocess.call(['/usr/bin/osascript', '-e', 'display dialog "An update is available for the Alfred Search Notes workflow. You can disable automatic update checks by editing the workflow.\n\nInformation about this release:\n\n%s" with title "Alfred Search Notes Workflow" buttons {"Cancel", "Download"} default button "Download" cancel button "Cancel"' % updateNotes, '2>/dev/null'])
     if retval == 0:
         return True
     else:
@@ -51,22 +48,21 @@ def update(updateUrl):
     '''
     updateFile = '/tmp/Search.Notes.alfredworkflow'
     # --location is required in order to follow redirects
-    curlRet = os.system('curl --silent --location --output %s %s' % (updateFile, updateUrl))
+    curlRet = subprocess.call(['/usr/bin/curl', '--silent', '--location', '--output', updateFile, updateUrl])
     openRet = 1
     if curlRet == 0:
-        openRet = os.system('open ' + updateFile)
+        openRet = subprocess.call(['/usr/bin/open', updateFile])
     if curlRet != 0 or openRet != 0:
-        os.system("osascript -e 'display alert \"Alfred Search Notes workflow failed to update.\" as critical' 2>/dev/null")
+        subprocess.call(['/usr/bin/osascript', '-e', 'display alert "Alfred Search Notes workflow failed to update." as critical', '2>/dev/null'])
     
     
 if oneDaySinceLastCheck():
     latestUrl = 'https://api.github.com/repos/sballin/alfred-search-notes-app/releases/latest'
-    latestFile = 'latest_release.json'
-    retval = os.system('curl --silent --max-time 30 --output %s %s' % (latestFile, latestUrl))
+    latestFile = '/tmp/search_notes_latest_release.json'
+    retval = subprocess.call(['/usr/bin/curl', '--silent', '--max-time', '30', '--output', latestFile, latestUrl])
     if retval == 0:
         with open(latestFile, 'r') as f:
             latest = json.load(f)
-        os.system('rm -f %s' % latestFile)
         latestVersion = latest['tag_name']
         updateNotes = latest['body']
         updateUrl = latest['assets'][0]['browser_download_url']
